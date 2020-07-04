a258bba96c6e827caa799493837532f12fe81ad2
Mimi Ojserkis <mimioj@gmail.com>
2020-07-04 17:40:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,10 @@
 def my_hash_creator(key, value)
- 
  new_hash = {key => value}
 end
 
 def read_from_hash(hash, key)
+  
+  hash = {key}
   # return the correct value using the hash and key parameters
 end
 
