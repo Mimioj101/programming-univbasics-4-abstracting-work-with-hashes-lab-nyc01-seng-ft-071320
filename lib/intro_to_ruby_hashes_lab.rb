82725ba4640e9c23a8728c4995fcc3e73a656166
Mimi Ojserkis <mimioj@gmail.com>
2020-07-04 17:55:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -11,9 +11,11 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   if
     hash[key] = false
-    key = 1 
+    hash[key] = 1 
   else
     hash[key] = true
     hash[key + 1]
   end
 end
+
+update_counting_hash( , "hello")
